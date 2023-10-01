4c80f743acdb92040d131fcedf4aee12be3d9e0b
Hstudent19 <h.sivalin@gmail.com>
2023-09-30 23:36:13 -0400

    second change

--- app/src/main/java/com/example/lab_2/MainActivity.java
+++ app/src/main/java/com/example/lab_2/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.lab_2;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,22 +19,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText text;
+       //button
+        EditText sub;
         Button btn;
         TextView change;
         change=(TextView)  findViewById(R.id.newtext);
-        text=(EditText)  findViewById(R.id.editTextText1);
+        sub=(EditText)  findViewById(R.id.editTextText1);
         btn= (Button) findViewById(R.id.pbutton);
+        //check box
+        CheckBox find;
 
-        text.setVisibility(View.INVISIBLE);
+        find =(CheckBox)  findViewById(R.id.rbcheckBox);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                text.setText("I love android");
-                text.setVisibility(View.VISIBLE);
+                change.setText(sub.getText());
+                Toast.makeText(getApplicationContext(),"Label has been changed ",Toast.LENGTH_LONG).show();
+
+
             }
+
+
+
         });
+
+
 
 
         }
